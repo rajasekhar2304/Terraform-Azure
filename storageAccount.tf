061187578e7920593729df882c1ee0868a1d420d
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-04-22 11:07:15 +0530

    creating storage account to store tf state file

---
+++ storageAccount.tf
@@ -0,0 +1,12 @@
+# Storage account for state file
+resource "azurerm_storage_account" "tfstatefile" {
+  name                     = "azb48prodtfstate"
+  resource_group_name      = "Terraform-RG"
+  location                 = "centralindia"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Production"
+  }
+}
