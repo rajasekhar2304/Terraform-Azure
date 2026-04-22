88f0da5c8273b2d278973e796e3612e0b1456611
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-04-22 11:13:19 +0530

    changing resource name of storage account

--- storageAccount.tf
+++ storageAccount.tf
@@ -1,5 +1,5 @@
 # Storage account for state file
-resource "azurerm_storage_account" "tfstatefile" {
+resource "azurerm_storage_account" "azb48prodtfstate" {
   name                     = "azb48prodtfstate"
   resource_group_name      = "Terraform-RG"
   location                 = "centralindia"
