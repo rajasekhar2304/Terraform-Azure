8323d780ffa7c1d0171c6719987bcfe26e1ac9aa
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-04-22 11:57:42 +0530

    creating subnets

--- vnet.tf
+++ vnet.tf
@@ -13,11 +13,11 @@ resource "azurerm_subnet" "vnet1-subnet-1" {
   name                 = "${azurerm_virtual_network.vnet1.name}-subnet-1"
   resource_group_name  = "${azurerm_resource_group.RG1.name}"
   virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefixes      = "10.0.1.0/24"
+  address_prefixes      = ["10.0.1.0/24"]
 }
 resource "azurerm_subnet" "vnet1-subnet-2" {
   name                 = "${azurerm_virtual_network.vnet1.name}-subnet-2"
   resource_group_name  = "${azurerm_resource_group.RG1.name}"
   virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  address_prefixes       = "10.0.2.0/24"
+  address_prefixes       = ["10.0.2.0/24"]
 }
