dc6aa70d170af3f3580ee2add9e8021169114c4c
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-04-22 12:05:26 +0530

    removing storage account from the state file

--- storageAccount.tf
+++ storageAccount.tf
@@ -1,18 +1,18 @@
 # Storage account for state file
-resource "azurerm_storage_account" "azb48prodtfstate" {
-  name                     = "azb48prodtfstate"
-  resource_group_name      = "Terraform-RG"
-  location                 = "centralindia"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "Production"
-  }
-}
-
-resource "azurerm_storage_container" "azb48prodtfstate" {
-  name                  = "tfstate"
-  storage_account_id  = azurerm_storage_account.azb48prodtfstate.id
-  container_access_type = "private"
-}
+#resource "azurerm_storage_account" "azb48prodtfstate" {
+#  name                     = "azb48prodtfstate"
+#  resource_group_name      = "Terraform-RG"
+#  location                 = "centralindia"
+#  account_tier             = "Standard"
+#  account_replication_type = "LRS"
+#
+#  tags = {
+#    environment = "Production"
+#  }
+#}
+#
+#resource "azurerm_storage_container" "azb48prodtfstate" {
+#  name                  = "tfstate"
+#  storage_account_id  = azurerm_storage_account.azb48prodtfstate.id
+#  container_access_type = "private"
+#}
