857e2b0c0636393407b0b10d03b08edbaabef8c4
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-04-22 22:12:07 +0530

    using varaible files

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "rg1_name" {}
+variable "location" {}
+variable "environment" {}
+variable "vnet1_address_space" {}
+variable "vnet1_subnet1_cidr" {}
+variable "vnet1_subnet2_cidr" {}
+variable "vnet1_subnet3_cidr" {}
