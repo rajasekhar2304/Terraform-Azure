959c8e043c06924622c04581ca13e99736d1ef0c
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2026-04-22 10:40:08 +0530

    Terraform Base Code

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "1.9.8"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.69.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ rg.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "RG1" {
+  name     = "RG1"
+  location = "eastus"
+
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_resource_group" "RG2" {
+  name     = "RG2"
+  location = "westus"
+
+  tags = {
+    environment = "Production"
+  }
+}
+resource "azurerm_resource_group" "RG3" {
+  name     = "RG3"
+  location = "ceindia"
+
+  tags = {
+    environment = "Production"
+  }
+}
